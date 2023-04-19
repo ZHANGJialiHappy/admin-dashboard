7e851f0ade9591b9aa3806a509f5c5f874580d5d
Jiali Zhang <zhangjiali86@gmail.com>
2023-04-19 16:20:24 +0200

    feat: if side view

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { BrowserRouter } from "react-router-dom";
+import './App.css';
+
 
 function App() {
-
+const activeMenu = false;
   return (
-    <div className="App">
-      <p>hi</p>
-<button className="btn">Button</button>
+    <div>
+      <BrowserRouter>
+      <div className="flex relative">
+      {activeMenu ? (
+         <div className="w-72 fixed sidebar bg-white">Sidebar</div>
+      ) : (
+        <div className="w-0"> Sidebar</div>
+      )}
+      <div className={`bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72': 'flex-2'}`}></div>
+
+      </div>
+
+      </BrowserRouter>
     </div>
   )
 }
