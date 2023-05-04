5b41e16aaaf38dc95b9b377d004532fc947a5103
Jiali Zhang <zhangjiali86@gmail.com>
2023-05-04 14:42:21 +0200

    feat: logo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,22 @@
 import {FaReact} from "react-icons/fa"
+import { Link } from "react-router-dom";
 
 function Sidebar() {
+  // const activeMenu = true;
   return (
-    <div>
-      Sidebar
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+      {/* {activeMenu && ( */}
+        <>
+        <div className="flex justify-between items-center">
+          <Link to="/" onClick = {() => {}}
+          className="flex items-center font-extrabold tracking-tight text-sky-400 gap-3 ml-3 mt-4">
+            <FaReact className="text-3xl"/>
+            <span className="text-xl">ReShop</span>
+          </Link>
+
+        </div>
+        </>
+      {/* )} */}
     </div>
   )
 }
