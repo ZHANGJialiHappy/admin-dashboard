7fdf809a06edf7aad89be861ec3b98d783ce407a
Jiali Zhang <zhangjiali86@gmail.com>
2023-05-06 13:36:21 +0200

    correct Button text color and bug of focus

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import { links } from "../data/dummy";
 
 function Sidebar() {
   // const activeMenu = true;
-  const activeLink = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2 focus:bg-sky-400";
+  const activeLink = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md m-2 focus:bg-sky-400 focus:text-sky-100 ";
   const normalLink = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 hover:bg-light-gray m-2"
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
